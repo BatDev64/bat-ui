97137673179cdd7949c5af07e77501dacc49dbb0
BatDev64 <bat.bits64@gmail.com>
2025-04-01 11:35:28 -0300

    feat(apps/bat-ui): add sotrybook addons

--- apps/bat-ui/.storybook/preview.ts
+++ apps/bat-ui/.storybook/preview.ts
@@ -1,7 +1,6 @@
 import type { Preview } from '@storybook/react'
 import './../src/global.css'
-/* import '@bat-ui/ui/css-theme'
-import '@bat-ui/ui/styles' */
+import { withThemeByDataAttribute } from '@storybook/addon-themes'
 
 const preview: Preview = {
   parameters: {
@@ -10,8 +9,26 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i
       }
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
     }
   }
 }
 
 export default preview
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light', // Clase CSS para el tema claro
+      dark: 'dark' // Clase CSS para el tema oscuro
+    },
+    defaultTheme: 'light', // Tema predeterminado,
+    attributeName: 'data-theme'
+  })
+]
